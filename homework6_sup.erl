919bda7dfca42c7ede3661c679cd054bf8307083
Biloshytskyi-Nikita <nexo.knight2003@gmail.com>
2024-12-06 02:56:50 +0200

    33

---
+++ homework6_sup.erl
@@ -0,0 +1,11 @@
+-module(homework6_sup).
+-behaviour(supervisor).
+
+-export([start_link/0, init/1]).
+
+start_link() ->
+    supervisor:start_link({local, homework6_sup}, homework6_sup, []).
+init([]) ->
+    {ok, {{one_for_one, 5, 10}, [
+        {homework6_server, {homework6_server, start_link, []}, permanent, 5000, worker, [homework6_server]}
+    ]}}.
